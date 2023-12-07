92c922292534fdd9469dffa70fece228bd85c51b
Sam Bencivengo <sambencivengo@gmail.com>
2023-12-07 10:49:17 -0700

    nav bar skeleton with links

---
+++ src/app/components/nav.tsx
@@ -0,0 +1,21 @@
+'use server';
+import React from 'react';
+import Link from 'next/link';
+
+export async function Nav() {
+  return (
+    <header className="sticky top-0 z-50">
+      <nav>
+        <div className="flex items-center justify-between p-2">
+          <Link href="/blog">BLOG</Link>
+          <Link href="/script">SCRIPT LIBRARY</Link>
+          <Link href="/video_library">VIDEO LIBRARY</Link>
+          <Link href="/">~COMPANY LOGO~</Link>
+          <Link href="/services">SERVICES (NOT WORKING)</Link>
+          <Link href="/about">ABOUT</Link>
+          <Link href="/contact">CONTACT</Link>
+        </div>
+      </nav>
+    </header>
+  );
+}
