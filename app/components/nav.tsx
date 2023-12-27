de3dacf90a775fac9f540932d004d6385d733cce
Sam Bencivengo <sambencivengo@gmail.com>
2023-12-26 19:06:15 -0700

    more nav adjustments'

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -5,22 +5,30 @@ import Image from 'next/image';
 
 export async function Nav() {
   return (
-    <header className="sticky top-0 z-50  mt-10 shadow-black shadow-2xl">
+    <header className="px-20 sticky w-8/12 top-0 z-50  mt-10 shadow-black shadow-2xl">
       <nav>
-        <div className="flex items-center justify-between p-2">
-          <div>
-            <Link href="/blog">BLOG</Link>
-            <Link href="/script">SCRIPT LIBRARY</Link>
-            <Link href="/video_library">VIDEO LIBRARY</Link>
-          </div>
-          <div className="px-20">
+        <div className="flex items-center justify-evenly p-2">
+          <Link className="text-sm" href="/blog">
+            BLOG
+          </Link>
+          <Link className="text-sm" href="/script">
+            SCRIPT LIBRARY
+          </Link>
+          <Link className="text-sm" href="/video_library">
+            VIDEO LIBRARY
+          </Link>
+          <div className="">
             <Image src="/main-logo.png" width={180} height={180} quality={100} alt="Lytic Group Logo" />
           </div>
-          <div>
-            <Link href="/services">SERVICES (NOT WORKING)</Link>
-            <Link href="/about">ABOUT</Link>
-            <Link href="/contact">CONTACT</Link>
-          </div>
+          <Link className="text-sm" href="/services">
+            SERVICES (NOT WORKING)
+          </Link>
+          <Link className="text-sm" href="/about">
+            ABOUT
+          </Link>
+          <Link className="text-sm" href="/contact">
+            CONTACT
+          </Link>
         </div>
       </nav>
     </header>
