e623df94003083adbf7632c2363adddf7e9fb728
Sam Bencivengo <sambencivengo@gmail.com>
2023-12-26 18:46:32 -0700

    logo and adjustments

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,19 +1,26 @@
 'use server';
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 
 export async function Nav() {
   return (
-    <header className="sticky top-0 z-50 w-7/12 shadow-black shadow-2xl">
+    <header className="sticky top-0 z-50  mt-10 shadow-black shadow-2xl">
       <nav>
         <div className="flex items-center justify-between p-2">
-          <Link href="/blog">BLOG</Link>
-          <Link href="/script">SCRIPT LIBRARY</Link>
-          <Link href="/video_library">VIDEO LIBRARY</Link>
-          <Link href="/">~COMPANY LOGO~</Link>
-          <Link href="/services">SERVICES (NOT WORKING)</Link>
-          <Link href="/about">ABOUT</Link>
-          <Link href="/contact">CONTACT</Link>
+          <div>
+            <Link href="/blog">BLOG</Link>
+            <Link href="/script">SCRIPT LIBRARY</Link>
+            <Link href="/video_library">VIDEO LIBRARY</Link>
+          </div>
+          <div className="px-20">
+            <Image src="/main-logo.png" width={180} height={180} quality={100} alt="Lytic Group Logo" />
+          </div>
+          <div>
+            <Link href="/services">SERVICES (NOT WORKING)</Link>
+            <Link href="/about">ABOUT</Link>
+            <Link href="/contact">CONTACT</Link>
+          </div>
         </div>
       </nav>
     </header>
