c3513ac3030c8e9b59b6ee55bba7423ddfadc0b5
Sam Bencivengo <sambencivengo@gmail.com>
2024-01-05 15:07:15 -0700

    futher nav updates

--- app/components/layout/nav.tsx
+++ app/components/layout/nav.tsx
@@ -5,32 +5,36 @@ import Image from 'next/image';
 
 export async function Nav() {
   return (
-    <header className="bg-white px-20 w-8/12 top-0 z-50 fixed shadow-black shadow-2xl">
+    <header className="bg-white w-10/12 top-0 z-50 fixed shadow-black shadow-2xl">
       <nav>
-        <div className="flex items-center justify-evenly p-2">
-          <Link className="text-sm" href="/blog">
-            BLOG
-          </Link>
-          <Link className="text-sm" href="/script">
-            SCRIPT LIBRARY
-          </Link>
-          <Link className="text-sm" href="/video_library">
-            VIDEO LIBRARY
-          </Link>
+        <div className="flex justify-between items-center py-4">
+          <div className="flex flex-row gap-4 pl-3">
+            <Link className="text-sm" href="/blog">
+              BLOG
+            </Link>
+            <Link className="text-sm" href="/script">
+              SCRIPT LIBRARY
+            </Link>
+            <Link className="text-sm" href="/video_library">
+              VIDEO LIBRARY
+            </Link>
+          </div>
           <div>
             <Link href="/">
               <Image src="/main-logo.png" width={180} height={180} quality={100} alt="Lytic Group Logo" />
             </Link>
           </div>
-          <Link className="text-sm" href="/services">
-            SERVICES (NOT WORKING)
-          </Link>
-          <Link className="text-sm" href="/about">
-            ABOUT
-          </Link>
-          <Link className="text-sm" href="/contact">
-            CONTACT
-          </Link>
+          <div className="flex justify-end  gap-4 pr-3">
+            <Link className="text-sm" href="/services">
+              SERVICES (NOT WORKING)
+            </Link>
+            <Link className="text-sm" href="/about">
+              ABOUT
+            </Link>
+            <Link className="text-sm" href="/contact">
+              CONTACT
+            </Link>
+          </div>
         </div>
       </nav>
     </header>
