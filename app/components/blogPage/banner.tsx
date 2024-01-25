7f724ff079c9a99e06549da115cf7d409607c8bb
Sam Bencivengo <sambencivengo@gmail.com>
2024-01-24 20:38:20 -0700

    title on banner image

--- app/components/blogPage/banner.tsx
+++ app/components/blogPage/banner.tsx
@@ -4,6 +4,11 @@ export const Banner = () => {
   return (
     <div className="w-full h-full relative flex items-center">
       <Image src="/blog-page-banner.png" layout="fill" objectFit="cover" quality={80} alt="Blog Page Banner image" />
+      <div className="absolute top-50 right-1/2 align-middle">
+        <div className="flex items-center text-center">
+          <p className="text-white text-5xl font-bold">PRESIDENT&apos;S BLOG</p>
+        </div>
+      </div>
     </div>
   );
 };
