dee26a3e87b0b1de3b28b87c7f6bcadbe0bb641a
Sam Bencivengo <sambencivengo@gmail.com>
2024-01-27 15:45:11 -0700

    gifs added

--- app/components/homePage/ourPeople.tsx
+++ app/components/homePage/ourPeople.tsx
@@ -1,8 +1,16 @@
 export const OurPeople = () => {
   return (
-    <div className="flex flex-col justify-center h-[432px]">
-      <p className="text-primary-blue text-4xl font-bold">OUR PEOPLE</p>
-      <p className="text-xl w-6/12">Our people and their people skills are the key to our success. Find out what it takes be on the Lytic Group team, and make our customers happy.</p>
+    <div className="flex flex-row justify-center items-center h-[432px]">
+      <img
+        src="/our-people-animated.gif"
+        style={{
+          height: '180px',
+        }}
+      />
+      <div>
+        <p className="text-primary-blue text-4xl font-bold">OUR PEOPLE</p>
+        <p className="text-xl w-6/12">Our people and their people skills are the key to our success. Find out what it takes be on the Lytic Group team, and make our customers happy.</p>
+      </div>
     </div>
   );
 };
--- app/components/homePage/presidentsBlog.tsx
+++ app/components/homePage/presidentsBlog.tsx
@@ -1,8 +1,16 @@
 export const PresidentsBlog = () => {
   return (
-    <div className="flex flex-col justify-center h-[432px]">
-      <p className="text-primary-green text-4xl font-bold">PRESIDENT&apos;S BLOG</p>
-      <p className="text-xl w-6/12">Thoughts on how to be a great consultant and an indispensable asset to a customer.</p>
+    <div className="flex flex-row justify-center items-center h-[432px]">
+      <img
+        src="/presidents-blog-animated.gif"
+        style={{
+          height: '180px',
+        }}
+      />
+      <div>
+        <p className="text-primary-green text-4xl font-bold">PRESIDENT&apos;S BLOG</p>
+        <p className="text-xl w-6/12">Thoughts on how to be a great consultant and an indispensable asset to a customer.</p>
+      </div>
     </div>
   );
 };
