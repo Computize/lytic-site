d30b85c93c93ecaab479171dbe6d6b0b6d47b478
Sam Bencivengo <sambencivengo@gmail.com>
2024-01-27 13:11:24 -0700

    carousel image

--- app/components/homePage/homePageCarousel.tsx
+++ app/components/homePage/homePageCarousel.tsx
@@ -1,10 +1,14 @@
 import { PageUpperImageContainer } from '~/app/components/pageUpperImageContainer';
-
+import Image from 'next/image';
 export const HomePageCarousel = () => {
   /* TODO: replace with carousel */
   return (
     <PageUpperImageContainer>
-      <p className="text-3xl">PLACEHOLDER FOR CAROUSEL</p>
+      <div>
+        <img src="/home-page-banner.png" className="object-cover h-[538px] w-full" />
+
+        {/* <Image layout="fill" quality={80} alt="Home Page Banner image" /> */}
+      </div>
     </PageUpperImageContainer>
   );
 };
