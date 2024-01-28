84dfd8e38235b2138c00d6f328a0b5c6531cc074
Sam Bencivengo <sambencivengo@gmail.com>
2024-01-28 12:02:48 -0700

    button for peopl

--- app/components/homePage/ourPeople.tsx
+++ app/components/homePage/ourPeople.tsx
@@ -11,7 +11,9 @@ export const OurPeople = () => {
         <div className="flex flex-col">
           <p className="text-primary-blue text-4xl font-bold">OUR PEOPLE</p>
           <p className="text-xl w-6/12">Our people and their people skills are the key to our success. Find out what it takes be on the Lytic Group team, and make our customers happy.</p>
-          <button></button>
+          <button className="bg-primary-blue hover:bg-blue-400 w-auto border-b-4 border-blue-200 text-white font-bold py-2 px-4 rounded-full">
+            <p className="text-black">MEET OUR TEAM</p>
+          </button>
         </div>
       </div>
     </div>
