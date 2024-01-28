e912760440e45f508598c1913c83ff7eee84b181
Sam Bencivengo <sambencivengo@gmail.com>
2024-01-28 12:31:09 -0700

    people blog sections on home page, small adjustments left over

--- app/components/homePage/ourPeople.tsx
+++ app/components/homePage/ourPeople.tsx
@@ -1,20 +1,21 @@
+import Link from 'next/link';
+
 export const OurPeople = () => {
   return (
-    <div className="flex justify-center items-center h-[400px] w-8/12">
-      <div className="flex flex-row gap-10">
-        <img
-          src="/our-people-animated.gif"
-          style={{
-            height: '180px',
-          }}
-        />
-        <div className="flex flex-col gap-12">
+    <div className="flex justify-center items-center h-[400px] w-full">
+      <div className="flex flex-row gap-10 ">
+        <div className="w-[200px]">
+          <img src="/our-people-animated.gif" style={{ width: 'full', height: 'full' }} />
+        </div>
+        <div className="flex flex-col gap-12 w-7/12">
           <p className="text-primary-blue text-4xl font-bold">OUR PEOPLE</p>
           <p className="text-xl w-auto">Our people and their people skills are the key to our success. Find out what it takes be on the Lytic Group team, and make our customers happy.</p>
           <div>
-            <button className="bg-primary-blue hover:bg-blue-400 w-auto border-b-4 border-blue-200 text-white font-bold py-2 px-4 rounded-full">
-              <p className="text-black">MEET OUR TEAM</p>
-            </button>
+            <Link href={'/about'}>
+              <button className="bg-primary-blue hover:bg-blue-400 w-auto border-b-4 border-blue-100 text-white font-bold py-2 px-4 rounded-full">
+                <p className="text-black">MEET OUR TEAM</p>
+              </button>
+            </Link>
           </div>
         </div>
       </div>
--- app/components/homePage/presidentsBlog.tsx
+++ app/components/homePage/presidentsBlog.tsx
@@ -1,16 +1,23 @@
+import Link from 'next/link';
+
 export const PresidentsBlog = () => {
   return (
-    <div className="flex flex-row justify-center items-center h-auto">
-      <img
-        src="/presidents-blog-animated.gif"
-        style={{
-          height: '180px',
-          width: 'auto',
-        }}
-      />
-      <div>
-        <p className="text-primary-green text-4xl font-bold">PRESIDENT&apos;S BLOG</p>
-        <p className="text-xl w-6/12">Thoughts on how to be a great consultant and an indispensable asset to a customer.</p>
+    <div className="flex justify-center items-center h-[400px] w-full">
+      <div className="flex flex-row gap-10 ">
+        <div className="w-[200px]">
+          <img src="/presidents-blog-animated.gif" style={{ width: 'full', height: 'full' }} />
+        </div>
+        <div className="flex flex-col gap-12 w-7/12">
+          <p className="text-primary-green text-4xl font-bold">PRESIDENT&apos;S BLOG</p>
+          <p className="text-xl w-auto">Thoughts on how to be a great consultant and an indispensable asset to a customer.</p>
+          <div>
+            <Link href={'/blog'}>
+              <button className="bg-primary-green hover:bg-green-400 w-auto border-b-4 border-green-100 text-white font-bold py-2 px-4 rounded-full">
+                <p className="text-black">READ THE BLOG</p>
+              </button>
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
