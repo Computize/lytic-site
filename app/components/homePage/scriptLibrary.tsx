3f5256f2505e32e021df752dc2fb10714e107ab8
Sam Bencivengo <sambencivengo@gmail.com>
2024-01-29 20:26:41 -0700

    background image better

--- app/components/homePage/scriptLibrary.tsx
+++ app/components/homePage/scriptLibrary.tsx
@@ -1,10 +1,17 @@
 export const ScriptLibrary = () => {
   return (
-    <div className="flex flex-col w-full bg-background-green justify-center h-[432px]">
-      <p className="text-white text-4xl font-bold">SCRIPT LIBRARY</p>
+    <div className="flex flex-col gap-10 w-auto h-[900px] px-36 bg-home-page-script-sec justify-center bg-cover bg-no-repeat" style={{
+      backgroundImage: "url('/home-page-script-sec.png')"
+    }}>
+      <p className="text-white text-5xl font-bold text-right">SCRIPT LIBRARY</p>
       <div className="text-gray-600 text-2xl font-bold">SHARE OUR TOOLKIT</div>
-      <p className="text-m">We’ve posted some SQL Server scripts that we’ve written, found particularly handy, and have used for our clients with great success. Every DBA should stop in and check them out.</p>
-      <p className="text-m font-bold">Coming Soon: Sharepoint Office 365!</p>
+      <div>
+        <p className="text-m">We’ve posted some SQL Server scripts that we’ve written, found particularly handy, and have used for our clients with great success. Every DBA should stop in and check them out.</p>
+        <p className="text-m font-bold">Coming Soon: Sharepoint Office 365!</p>
+      </div>
+      <div className="text-right">
+        <button className="bg-primary-blue hover:bg-blue-400 w-auto border-b-4 border-blue-100 text-black font-bold py-4 px-4 rounded-full">SEE ALL SCRIPTS</button>
+      </div>
     </div>
   );
 };
