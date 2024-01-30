d418b7382e7a3c5fcb54c548c8700d1d70c24c28
Sam Bencivengo <sambencivengo@gmail.com>
2024-01-29 20:32:47 -0700

    testimonials updated

--- app/components/homePage/testimonials.tsx
+++ app/components/homePage/testimonials.tsx
@@ -1,8 +1,15 @@
 export const Testimonials = () => {
   return (
-    <div className="flex flex-col justify-center h-[732px]">
-      <p className="text-primary-blue text-5xl font-bold">TESTIMONIALS</p>
-      <p className="text-xl w-6/12">{'{TESTIMONIALS CAROUSEL WILL GO HERE'}</p>
+    <div
+      className="flex flex-col justify-center w-full h-[932px] bg-no-repeat bg-contain"
+      style={{
+        backgroundImage: "url('/testimonial-bg.jpg')",
+      }}
+    >
+      <div className="">
+        <p className="text-primary-blue text-5xl font-bold">TESTIMONIALS</p>
+        <p className="text-xl w-6/12">{'{TESTIMONIALS CAROUSEL WILL GO HERE'}</p>
+      </div>
     </div>
   );
 };
