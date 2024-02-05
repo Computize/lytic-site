fa09368fa7eb16b3297a5d88cfe92f4d00a94385
Sam Bencivengo <sambencivengo@gmail.com>
2024-02-04 21:01:48 -0700

    padding inputs

--- app/components/blogPage/getOurNewsletter.tsx
+++ app/components/blogPage/getOurNewsletter.tsx
@@ -4,7 +4,7 @@ export const GetOurNewsletter = () => {
       <div className="min-w-full py-11 flex flex-col gap-5 items-center bg-background-light-gray">
         <p className="text-5xl font-bold text-primary-green">GET OUR NEWSLETTER</p>
         <div className="flex flex-row gap-4">
-          <input className="w-[800px] h-11 placeholder:italic" placeholder="Enter your email address" />
+          <input className="w-[800px] h-11 placeholder:italic pl-3" placeholder="Enter your email address" />
           <button className="bg-primary-green">SIGN ME UP</button>
         </div>
       </div>
--- app/components/ourApproachPage/letsTalk.tsx
+++ app/components/ourApproachPage/letsTalk.tsx
@@ -3,7 +3,7 @@ export const LetsTalk = () => {
     <div className="flex flex-col items-center justify-center gap-5 w-full bg-primary-green h-52 mb-10">
       <p className="text-5xl font-bold text-white">GET OUR NEWSLETTER</p>
       <div className="flex flex-row gap-4">
-        <input className="w-[800px] h-11 placeholder:italic" placeholder="Enter your email address" />
+        <input className="w-[800px] h-11 placeholder:italic pl-3" placeholder="Enter your email address" />
         <button className="bg-secondary-green px-9 text-white font-bold">CONTACT ME</button>
       </div>
     </div>
