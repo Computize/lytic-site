f6b35b495f90ec5bd8d2189f81da9ccc34a01e86
Sam Bencivengo <sambencivengo@gmail.com>
2024-02-06 16:08:08 -0700

    select added

--- app/components/scriptPage/scriptDropDown.tsx
+++ app/components/scriptPage/scriptDropDown.tsx
@@ -1,3 +1,23 @@
+import { Select, SelectTrigger, SelectValue, SelectContent, SelectGroup, SelectLabel, SelectItem } from '~/components/ui/select';
+
 export const ScriptDropDown = () => {
-  return <div>PLACEHOLDER DROPDOWN FOR SCRIPTS</div>;
+  return (
+    <div>
+      <Select>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Select a script" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectLabel>Fruits</SelectLabel>
+            <SelectItem value="apple">Apple</SelectItem>
+            <SelectItem value="banana">Banana</SelectItem>
+            <SelectItem value="blueberry">Blueberry</SelectItem>
+            <SelectItem value="grapes">Grapes</SelectItem>
+            <SelectItem value="pineapple">Pineapple</SelectItem>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    </div>
+  );
 };
