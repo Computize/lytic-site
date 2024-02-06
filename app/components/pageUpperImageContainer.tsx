a68ba3d7f4b9243af9586deec190c3dc06bb446b
Sam Bencivengo <sambencivengo@gmail.com>
2024-02-06 11:12:50 -0700

    pageUpper image container adjust to work smarter

--- app/components/pageUpperImageContainer.tsx
+++ app/components/pageUpperImageContainer.tsx
@@ -4,7 +4,19 @@ import React from 'react';
 
 interface PageUpperImageContainerProps {
   children: React.ReactNode;
+  imageSource: string;
 }
-export const PageUpperImageContainer = ({ children }: PageUpperImageContainerProps) => {
-  return <div className="min-w-full h-[462px]">{children}</div>;
+export const PageUpperImageContainer = ({ children, imageSource }: PageUpperImageContainerProps) => {
+  return (
+    <div className="min-w-full h-[462px]">
+      <div
+        className="w-full h-full flex items-center justify-center"
+        style={{
+          backgroundImage: `url('${imageSource}')`,
+        }}
+      >
+        {children}
+      </div>
+    </div>
+  );
 };
--- app/starter_packages/page.tsx
+++ app/starter_packages/page.tsx
@@ -3,16 +3,9 @@ import { PageUpperImageContainer } from '~/app/components/pageUpperImageContaine
 export default function Page() {
   return (
     <main className="flex flex-col w-full">
-      <PageUpperImageContainer>
-        <div
-          className="w-full h-full flex items-center justify-center"
-          style={{
-            backgroundImage: "url('/star_packeges-banner.png')",
-          }}
-        >
-          <div className="flex items-center text-center">
-            <p className="text-white text-5xl font-bold">STARTER PACKAGES</p>
-          </div>
+      <PageUpperImageContainer imageSource="/star_packeges-banner.png">
+        <div className="flex items-center text-center">
+          <p className="text-white text-5xl font-bold">STARTER PACKAGES</p>
         </div>
       </PageUpperImageContainer>
     </main>
