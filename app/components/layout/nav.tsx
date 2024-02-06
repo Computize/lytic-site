97ec48333e85355182f47e82932e526f585a9b9c
Sam Bencivengo <sambencivengo@gmail.com>
2024-02-06 10:55:55 -0700

    conditional gradient

--- app/components/layout/nav.tsx
+++ app/components/layout/nav.tsx
@@ -16,7 +16,7 @@ export function Nav() {
     };
   }, []);
   return (
-    <div className="w-full top-0 z-50 h-36 fixed flex justify-center bg-gradient-to-b from-white">
+    <div className={`${isScrolled ? 'bg-gradient-to-b from-white' : ''}w-full top-0 z-50 h-36 fixed flex justify-center`}>
       <header className={isScrolled ? 'bg-white w-10/12 top-0 z-50 fixed shadow-black shadow-2xl' : 'w-10/12 top-0 z-50 fixed'}>
         <nav>
           <div className="text-xs font-bold flex justify-evenly items-center  w-full py-4">
