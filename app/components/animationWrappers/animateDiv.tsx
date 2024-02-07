439d7c40c14041bffdee31a26b73841df8b2f99e
Sam Bencivengo <sambencivengo@gmail.com>
2024-02-07 16:25:09 -0700

    carousel color and animation

--- app/components/animationWrappers/animateDiv.tsx
+++ app/components/animationWrappers/animateDiv.tsx
@@ -6,13 +6,14 @@ interface AnimateDivProps {
   children: React.ReactNode;
   initialProps: AnimationProps['initial'];
   duration?: number;
+  delay?: number;
 }
-export const AnimateDiv = ({ children, initialProps, duration }: AnimateDivProps) => {
+export const AnimateDiv = ({ children, initialProps, duration, delay }: AnimateDivProps) => {
   return (
     <motion.div
       initial={initialProps}
       viewport={{ once: true }}
-      whileInView={{ opacity: 1, translateX: 0, translateY: 0, transition: { duration: duration ?? 1 } }}
+      whileInView={{ opacity: 1, translateX: 0, translateY: 0, transition: { duration: duration ?? 1, delay: delay ?? 0 } }}
     >
       {children}
     </motion.div>
--- app/components/homePage/homePageCarousel.tsx
+++ app/components/homePage/homePageCarousel.tsx
@@ -1,22 +1,27 @@
+import { AnimateDiv } from '~/app/components/animationWrappers/animateDiv';
 import { Button } from '~/components/ui/button';
-import { Carousel, CarouselContent, CarouselItem, CarouselPrevious, CarouselNext } from '~/components/ui/carousel';
+import { Carousel, CarouselContent, CarouselItem } from '~/components/ui/carousel';
 
-const arrayOfSlides: Array<{ title: string; description: string }> = [
+const arrayOfSlides: Array<{ title: string; description: string; colorBlend: string }> = [
+  {
+    title: 'BUSINESS INTELLIGENCE',
+    description: 'Re-focus your business to data-based decision-making. Let the Lytic Group turn your raw data into usable information.',
+    colorBlend: '',
+  },
   {
     title: 'ENGAGEMENT',
     description: "It's the soft skills that determine your comfort level and satisfaction with the consultants that you’ve hired.",
+    colorBlend: 'bg-yellow-900',
   },
   {
     title: 'TAKING CARE',
     description: 'More than you have likely seen, the Lytic Group treats our customers’ projects, employees, and most of all their time, with the utmost respect. This comes baked-in to how we work and how we communicate.',
+    colorBlend: 'bg-blue-900',
   },
   {
     title: 'YOUR FAVORITE PROJECT',
     description: "It's the soft skills that determine your comfort level and satisfaction with the consultants that you’ve hired.",
-  },
-  {
-    title: 'BUSINESS INTELLIGENCE',
-    description: 'Re-focus your business to data-based decision-making. Let the Lytic Group turn your raw data into usable information.',
+    colorBlend: 'bg-green-900',
   },
 ];
 
@@ -29,10 +34,10 @@ export const HomePageCarousel = () => {
         }}
       >
         <CarouselContent className="">
-          {arrayOfSlides.map(({ description, title }, index) => {
+          {arrayOfSlides.map(({ description, title, colorBlend }, index) => {
             return (
               <CarouselItem
-                className="min-w-full h-[562px]"
+                className={`min-w-full h-[562px] bg-blend-screen ${colorBlend}`}
                 key={index}
                 style={{
                   backgroundImage: "url('/home-page-banner.png')",
@@ -42,41 +47,33 @@ export const HomePageCarousel = () => {
                   <div className="h-full w-2/12" />
                   <div className="text-white flex flex-col justify-evenly">
                     <div className="h-9" />
-                    <p className="text-4xl font-bold">{title}</p>
+                    <AnimateDiv
+                      initialProps={{ opacity: '0', translateY: -50 }}
+                      delay={0.75}
+                    >
+                      <p className="text-4xl font-bold">{title}</p>
+                    </AnimateDiv>
                     <div className="h-9" />
-                    <p className="text-white text-2xl">{description}</p>
+                    <AnimateDiv
+                      initialProps={{ opacity: '0', translateX: -100 }}
+                      delay={0.75}
+                    >
+                      <p className="text-white text-2xl">{description}</p>
+                    </AnimateDiv>
                     <div className="h-9" />
-                    <Button>READ MORE</Button>
+                    <AnimateDiv
+                      initialProps={{ opacity: '0', translateY: 50 }}
+                      delay={0.75}
+                    >
+                      <Button>READ MORE</Button>
+                    </AnimateDiv>
                   </div>
                   <div className="h-full w-2/12" />
                 </div>
               </CarouselItem>
             );
           })}
-          {/* {Array.from({ length: 5 }).map((_, index) => (
-            <CarouselItem
-              className="min-w-full h-[562px]"
-              key={index}
-              style={{
-                backgroundImage: "url('/home-page-banner.png')",
-              }}
-            >
-              <div className="h-full w-full flex justify-between items-center">
-                <div className="h-full w-2/12" />
-                <div>
-                  <p>YOUR FAVORITE PROJECT</p>
-                  <p className="text-white text-2xl">
-                    With remarkable transparency and unusually direct access <br /> to the team working for you, you’ll wonder why no <br /> other consulting group has been able to deliver so well.
-                  </p>
-                </div>
-                <div className="h-full w-2/12" />
-              </div>
-              <button>READ MORE</button>
-            </CarouselItem>
-          ))} */}
         </CarouselContent>
-        {/* <CarouselPrevious /> */}
-        {/* <CarouselNext /> */}
       </Carousel>
     </div>
   );
