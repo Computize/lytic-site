74b780035722a5ffdd655f2a29b9fcfe08456944
Sam Bencivengo <sambencivengo@gmail.com>
2024-02-07 11:11:02 -0700

    further updates to image container and blurb

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,22 +2,18 @@
 import { OurMission } from '~/app/components/about/ourMission';
 import { OurStory } from '~/app/components/about/ourStory';
 import { SomeOfOurClients } from '~/app/components/about/someOfOurClients';
+import { PageBlurb } from '~/app/components/pageBlurb';
 import { PageUpperImageContainer } from '~/app/components/pageUpperImageContainer';
 
 export default async function Page() {
+  const blurbString = 'The Lytic Group are architects of data \n and business intelligence solutions. \n Planning, surfacing, optimizing, & monitoring \n your most valuable data.';
   return (
     <main className="mb-14">
-      <PageUpperImageContainer>
-        <div
-          className="w-full h-full flex items-center"
-          style={{
-            backgroundImage: "url('/aboutus-page-banner.png')",
-          }}
-        >
-          <p className="text-4xl text-white max-w-6xl mx-[200px] font-bold">
-            The Lytic Group are architects of data <br /> and business intelligence solutions. <br /> Planning, surfacing, optimizing, & monitoring <br /> your most valuable data.
-          </p>
-        </div>
+      <PageUpperImageContainer imageSource="/aboutus-page-banner.png">
+        <PageBlurb
+          className="drop-shadow-lg text-4xl text-left max-w-3xl"
+          blurb={blurbString}
+        />
       </PageUpperImageContainer>
       <div className="flex justify-center">
         <OurStory />
--- app/video_library/page.tsx
+++ app/video_library/page.tsx
@@ -1,19 +1,16 @@
 import { VideosContainer } from '~/app/components/videoLibrary/videosContainer';
 import { VideoLibraryDescription } from '~/app/components/videoLibrary/videoLibraryDescription';
 import { PageTitle } from '~app/components/pageTitle';
+import { PageUpperImageContainer } from '~/app/components/pageUpperImageContainer';
 
 export default async function Page() {
   return (
     <main>
-      <div className="flex flex-col gap-8 py-16">
-        <div className="min-w-full h-[350px]">
-          <div className="flex justify-center items-center h-full bg-gradient-to-t from-primary-green via-primary-green to-white">
-            <PageTitle title="VIDEO LIBRARY" />
-          </div>
-        </div>
-        <VideoLibraryDescription />
-        <VideosContainer />
-      </div>
+      <PageUpperImageContainer>
+        <PageTitle title="VIDEO LIBRARY" />
+      </PageUpperImageContainer>
+      <VideoLibraryDescription />
+      <VideosContainer />
     </main>
   );
 }
