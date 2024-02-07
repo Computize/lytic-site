844a5c674c830aadd45d84e39861bc130e5f2d34
Sam Bencivengo <sambencivengo@gmail.com>
2024-02-07 10:44:29 -0700

    president's blog animation

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,15 +2,14 @@ import { Banner } from '~/app/components/blogPage/banner';
 import { BlogCategories } from '~/app/components/blogPage/blogCategories';
 import { BlogPreviewCard } from '~/app/components/blogPage/blogPreviewCard';
 import { GetOurNewsletter } from '~/app/components/blogPage/getOurNewsletter';
+import { PageTitle } from '~/app/components/pageTitle';
 import { PageUpperImageContainer } from '~/app/components/pageUpperImageContainer';
 
 export default async function Page() {
   return (
     <main className="flex flex-col w-full">
       <PageUpperImageContainer imageSource="/blog-page-banner.png">
-        <div className="flex items-center text-center">
-          <p className="text-white text-5xl font-bold">PRESIDENT&apos;S BLOG</p>
-        </div>
+        <PageTitle title="PRESIDENT'S BLOG" />
       </PageUpperImageContainer>
       <div></div>
       <div className="p-10 flex flex-row justify-center gap-5">
