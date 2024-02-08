63b8ae90cca117cdab9813bc81540a67a7ab528d
Sam Bencivengo <sambencivengo@gmail.com>
2024-02-07 17:59:36 -0700

    script library animation

--- app/components/homePage/ourPeoplePresidentsBlog.tsx
+++ app/components/homePage/ourPeoplePresidentsBlog.tsx
@@ -1,3 +1,5 @@
+import { AnimateDiv } from '~/app/components/animationWrappers/animateDiv';
+
 export const OurPeoplePresidentsBlog = () => {
   return (
     <div className="flex flex-col items-center w-10/12 h-[600px] gap-12">
@@ -9,15 +11,17 @@ export const OurPeoplePresidentsBlog = () => {
           />
         </div>
         <div className="w-8" />
-        <div className="flex flex-col gap-10 max-w-[500px]">
-          <p className="text-primary-blue text-5xl font-bold">OUR PEOPLE</p>
-          <p className="text-2xl w-auto">Our people and their people skills are the key to our success. Find out what it takes be on the Lytic Group team, and make our customers happy.</p>
-          <div className="flex">
-            <button className="bg-primary-blue hover:bg-blue-400 w-auto border-b-4 border-blue-100 text-white font-bold py-2 px-4 rounded-full">
-              <p className="text-black">MEET OUR TEAM</p>
-            </button>
+        <AnimateDiv initialProps={{ opacity: 0, translateX: 120 }}>
+          <div className="flex flex-col gap-10 max-w-[500px]">
+            <p className="text-primary-blue text-5xl font-bold">OUR PEOPLE</p>
+            <p className="text-2xl w-auto">Our people and their people skills are the key to our success. Find out what it takes be on the Lytic Group team, and make our customers happy.</p>
+            <div className="flex">
+              <button className="bg-primary-blue hover:bg-blue-400 w-auto border-b-4 border-blue-100 text-white font-bold py-2 px-4 rounded-full">
+                <p className="text-black">MEET OUR TEAM</p>
+              </button>
+            </div>
           </div>
-        </div>
+        </AnimateDiv>
       </div>
       <div className="min-h-[300px] flex flex-row justify-start">
         <div className="w-[200px]">
@@ -27,15 +31,17 @@ export const OurPeoplePresidentsBlog = () => {
           />
         </div>
         <div className="w-8" />
-        <div className="flex flex-col gap-10 max-w-[500px]">
-          <p className="text-primary-green text-5xl font-bold">PRESIDENT&apos;S BLOG</p>
-          <p className="text-2xl w-auto">Thoughts on how to be a great consultant and an indispensable asset to a customer.</p>
-          <div className="flex">
-            <button className="bg-primary-green hover:bg-secondary-green w-auto border-b-4 border-green-100 font-bold text-white py-2 px-4 rounded-full">
-              <p className="text-black">READ THE BLOG</p>
-            </button>
+        <AnimateDiv initialProps={{ opacity: 0, translateX: 120 }}>
+          <div className="flex flex-col gap-10 max-w-[500px]">
+            <p className="text-primary-green text-5xl font-bold">PRESIDENT&apos;S BLOG</p>
+            <p className="text-2xl w-auto">Thoughts on how to be a great consultant and an indispensable asset to a customer.</p>
+            <div className="flex">
+              <button className="bg-primary-green hover:bg-secondary-green w-auto border-b-4 border-green-100 font-bold text-white py-2 px-4 rounded-full">
+                <p className="text-black">READ THE BLOG</p>
+              </button>
+            </div>
           </div>
-        </div>
+        </AnimateDiv>
       </div>
     </div>
   );
--- app/components/homePage/scriptLibrary.tsx
+++ app/components/homePage/scriptLibrary.tsx
@@ -1,3 +1,5 @@
+import { AnimateDiv } from '~/app/components/animationWrappers/animateDiv';
+
 export const ScriptLibrary = () => {
   return (
     <div
@@ -6,15 +8,22 @@ export const ScriptLibrary = () => {
         backgroundImage: "url('/home-page-script-sec.png')",
       }}
     >
-      <p className="text-white text-5xl font-bold text-right">SCRIPT LIBRARY</p>
-      <div className="text-gray-600 text-2xl font-bold">SHARE OUR TOOLKIT</div>
-      <div>
-        <p className="text-m">We’ve posted some SQL Server scripts that we’ve written, found particularly handy, and have used for our clients with great success. Every DBA should stop in and check them out.</p>
-        <p className="text-m font-bold">Coming Soon: Sharepoint Office 365!</p>
-      </div>
-      <div className="text-right">
-        <button className="bg-primary-blue hover:bg-blue-400 w-auto border-b-4 border-blue-100 text-black font-bold py-4 px-4 rounded-full">SEE ALL SCRIPTS</button>
-      </div>
+      <AnimateDiv initialProps={{ opacity: 0, translateX: 75 }}>
+        <p className="text-white text-5xl font-bold text-right">SCRIPT LIBRARY</p>
+      </AnimateDiv>
+
+      <AnimateDiv initialProps={{ opacity: 0, translateX: -75 }}>
+        <div className="text-gray-600 text-2xl font-bold">SHARE OUR TOOLKIT</div>
+        <div>
+          <p className="text-m">We’ve posted some SQL Server scripts that we’ve written, found particularly handy, and have used for our clients with great success. Every DBA should stop in and check them out.</p>
+          <p className="text-m font-bold">Coming Soon: Sharepoint Office 365!</p>
+        </div>
+      </AnimateDiv>
+      <AnimateDiv initialProps={{ opacity: 0, translateX: 75 }}>
+        <div className="text-right">
+          <button className="bg-primary-blue hover:bg-blue-400 w-auto border-b-4 border-blue-100 text-black font-bold py-4 px-4 rounded-full">SEE ALL SCRIPTS</button>
+        </div>
+      </AnimateDiv>
     </div>
   );
 };
