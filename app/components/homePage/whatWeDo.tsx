657425e38ffea2f698e59a90c3241404134ba203
Sam Bencivengo <sambencivengo@gmail.com>
2024-02-08 16:52:46 -0700

    what we do

--- app/components/homePage/whatWeDo.tsx
+++ app/components/homePage/whatWeDo.tsx
@@ -1,13 +1,17 @@
 import Image from 'next/image';
 export const WhatWeDo = () => {
   return (
-    <div className="flex flex-row justify-end h-[732px] py-36">
-      <div className="animate-slide-right">
-        <p className="text-primary-green text-4xl font-bold">WHAT WE DO</p>
-        <p className="text-xl w-6/12">Specializing in Microsoft&apos;s analytics and data storage platform, high performance data warehouses and data lakes, and Microsoft&apos;s user-friendly ecosystem to surface it. We deliver on customized architecture that puts actionable data in your hands.</p>
+    <div className="flex flex-col md:flex-row items-center justify-between h-auto gap-7 py-20">
+      <div className="flex flex-col gap-3 md:gap-10 px-6 md:ml-40">
+        <p className="text-primary-green text-4xl md:text-6xl font-bold">WHAT WE DO</p>
+        <p className="text-l md:text-xl w-10/12">Specializing in Microsoft&apos;s analytics and data storage platform, high performance data warehouses and data lakes, and Microsoft&apos;s user-friendly ecosystem to surface it. We deliver on customized architecture that puts actionable data in your hands.</p>
       </div>
-      <div>
-        <Image src="/what-we-do-main-img.png" width={1000} height={1000} quality={100} alt="Lytic Group Logo" />
+      <div className="w-full flex justify-end">
+        <img
+          src="/what-we-do-main-img.png"
+          alt="Lytic Group Logo"
+          className="w-[300px] md:w-[2000px]"
+        />
       </div>
     </div>
   );
