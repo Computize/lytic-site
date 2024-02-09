dc93ba0924a0c918a82139ccccf1a764e14f5798
Sam Bencivengo <sambencivengo@gmail.com>
2024-02-08 18:48:32 -0700

    foorter update

--- app/components/layout/footer.tsx
+++ app/components/layout/footer.tsx
@@ -7,26 +7,50 @@ export async function Footer() {
   return (
     <footer className="h-[355px] py-[60px] flex flex-col gap-4 items-center  bg-background-dark-gray">
       <Link href="/">
-        <Image src="/main-logo.png" width={280} height={180} quality={100} alt="Lytic Group Logo" />
+        <Image
+          src="/main-logo.png"
+          width={280}
+          height={180}
+          quality={100}
+          alt="Lytic Group Logo"
+        />
       </Link>
 
-      <div className="flex flex-row text-primary-gray gap-6 items-center justify-evenly">
-        <Link className="text-sm" href="/blog">
+      <div className="flex flex-col sm:flex-row text-primary-gray gap-6 items-center justify-evenly">
+        <Link
+          className="text-sm"
+          href="/blog"
+        >
           BLOG
         </Link>
-        <Link className="text-sm" href="/script">
+        <Link
+          className="text-sm"
+          href="/script"
+        >
           SCRIPT LIBRARY
         </Link>
-        <Link className="text-sm" href="/video_library">
+        <Link
+          className="text-sm"
+          href="/video_library"
+        >
           VIDEO LIBRARY
         </Link>
-        <Link className="text-sm" href="/services">
+        <Link
+          className="text-sm"
+          href="/services"
+        >
           SERVICES (NOT WORKING)
         </Link>
-        <Link className="text-sm" href="/about">
+        <Link
+          className="text-sm"
+          href="/about"
+        >
           ABOUT
         </Link>
-        <Link className="text-sm" href="/contact">
+        <Link
+          className="text-sm"
+          href="/contact"
+        >
           CONTACT
         </Link>
       </div>
