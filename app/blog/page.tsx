57a0d5d5200fec478c20e6316fb360f5660db26a
Sam Bencivengo <sambencivengo@gmail.com>
2024-02-09 15:22:19 -0700

    blog css

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -8,11 +8,14 @@ export default async function Page() {
   return (
     <main className="flex flex-col w-full">
       <PageUpperImageContainer imageSource="/blog-page-banner.png">
-        <PageTitle title="PRESIDENT'S BLOG" />
+        <PageTitle
+          title="PRESIDENT'S BLOG"
+          className="text-center"
+        />
       </PageUpperImageContainer>
       <div></div>
-      <div className="p-10 flex flex-row justify-center gap-5">
-        <div className="grid grid-cols-2 gap-4">
+      <div className="p-10 flex flex-col md:flex-row justify-center gap-5">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           <BlogPreviewCard />
           <BlogPreviewCard />
           <BlogPreviewCard />
