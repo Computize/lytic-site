3da91a07af70893a7c1619ef606ae8660874a9b5
Sam Bencivengo <sambencivengo@gmail.com>
2024-02-08 18:11:15 -0700

    our approach mobile styling

--- app/components/homePage/ourApproach.tsx
+++ app/components/homePage/ourApproach.tsx
@@ -3,7 +3,7 @@ import { AnimateDiv } from '~/app/components/animationWrappers/animateDiv';
 export const OurApproach = () => {
   return (
     <div
-      className="w-full h-[400px] flex justify-center items-center "
+      className="w-full flex justify-center items-center px-6"
       style={{
         backgroundImage: "url('/our-approach-bg.png')",
         backgroundRepeat: 'no-repeat',
@@ -14,13 +14,13 @@ export const OurApproach = () => {
     >
       <div className="flex flex-col justify-center items-center gap-7 z-10">
         <AnimateDiv initialProps={{ opacity: 0, translateY: 50 }}>
-          <p className="text-primary-green text-center text-5xl font-bold">OUR APPROACH</p>
+          <p className="text-primary-green text-center text-4xl md:text-5xl font-bold">OUR APPROACH</p>
         </AnimateDiv>
         <AnimateDiv
           initialProps={{ opacity: 0, translateY: 50 }}
           delay={0.5}
         >
-          <p className="text-2xl w-[700px] text-center">From unheard-of transparency to reliable communication, read on to find out how we create the best consulting experience your organization has ever had.</p>
+          <p className="text-2xl w-full text-center">From unheard-of transparency to reliable communication, read on to find out how we create the best consulting experience your organization has ever had.</p>
         </AnimateDiv>
       </div>
     </div>
