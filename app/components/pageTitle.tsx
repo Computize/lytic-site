eaf72a47b3d1e0822e21fa125d79ef97ba09aaef
Sam Bencivengo <sambencivengo@gmail.com>
2024-02-08 19:35:14 -0700

    script page

--- app/components/pageTitle.tsx
+++ app/components/pageTitle.tsx
@@ -1,9 +1,12 @@
 'use server';
 
+import { cn } from '~/lib/utils';
+
 interface PageTitleProps {
   title: string;
   blurb?: boolean;
+  className?: string;
 }
-export const PageTitle = ({ title, blurb }: PageTitleProps) => {
-  return <p className={`text-white font-bold animate-slideInFromBottom ${blurb ? 'text-3xl' : 'text-5xl'}`}>{title.toUpperCase()}</p>;
+export const PageTitle = ({ title, blurb, className }: PageTitleProps) => {
+  return <p className={cn(`text-white font-bold animate-slideInFromBottom ${blurb ? 'text-3xl' : 'text-5xl'}`, className)}>{title.toUpperCase()}</p>;
 };
--- app/components/scriptPage/needMoreHelpContainer.tsx
+++ app/components/scriptPage/needMoreHelpContainer.tsx
@@ -3,8 +3,11 @@ import { PageTitle } from '~/app/components/pageTitle';
 
 export const NeedMoreHelpContainer = () => {
   return (
-    <div className="w-full h-auto mb-6 bg-gray-400">
-      <PageTitle title="NEED MORE HELP?" />
+    <div className="flex flex-col gap-8 bg-[url('/script-btm-section-bg.png')] bg-no-repeat bg-cover items-center min-h-[300px] justify-center">
+      <PageTitle
+        title="NEED MORE HELP?"
+        className="text-center"
+      />
       <Link href="/contact">
         <button className="bg-primary-green w- p-2 px-12 text-white text-lg font-bold">CONTACT</button>
       </Link>
