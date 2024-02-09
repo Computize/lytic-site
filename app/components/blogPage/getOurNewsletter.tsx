5035fc155550febdc1574ca4e62096b09f9741fa
Sam Bencivengo <sambencivengo@gmail.com>
2024-02-09 15:27:13 -0700

    get our newsletter

--- app/components/blogPage/getOurNewsletter.tsx
+++ app/components/blogPage/getOurNewsletter.tsx
@@ -1,11 +1,16 @@
+import { Button } from '~/components/ui/button';
+
 export const GetOurNewsletter = () => {
   return (
     <div className="py-5">
-      <div className="min-w-full py-11 flex flex-col gap-5 items-center bg-background-light-gray">
-        <p className="text-5xl font-bold text-primary-green">GET OUR NEWSLETTER</p>
-        <div className="flex flex-row gap-4">
-          <input className="w-[800px] h-11 placeholder:italic pl-3" placeholder="Enter your email address" />
-          <button className="bg-primary-green">SIGN ME UP</button>
+      <div className="py-11 flex flex-col gap-5 items-center bg-background-light-gray">
+        <p className="text-5xl font-bold text-center text-primary-green">GET OUR NEWSLETTER</p>
+        <div className="flex flex-col justify-center md:flex-row gap-4">
+          <input
+            className="w-[300px] md:w-[500px] h-11 placeholder:italic pl-3 "
+            placeholder="Enter your email address"
+          />
+          <Button className="bg-primary-green text rounded-none">SIGN ME UP</Button>
         </div>
       </div>
     </div>
