4be140347d57345109c310fc8c5c11bfaddf45ea
Sam Bencivengo <sambencivengo@gmail.com>
2024-02-08 18:30:40 -0700

    script library

--- app/components/homePage/scriptLibrary.tsx
+++ app/components/homePage/scriptLibrary.tsx
@@ -3,20 +3,24 @@ import { AnimateDiv } from '~/app/components/animationWrappers/animateDiv';
 export const ScriptLibrary = () => {
   return (
     <div
-      className="flex flex-col gap-10 w-auto h-[900px] px-36 bg-home-page-script-sec justify-center bg-cover bg-no-repeat"
+      className="flex flex-col gap-10 h-[800px] w-full p-4 md:px-36 bg-home-page-script-sec justify-center bg-cover bg-no-repeat"
       style={{
+        objectFit: 'contain',
+        width: 'auto',
         backgroundImage: "url('/home-page-script-sec.png')",
       }}
     >
       <AnimateDiv initialProps={{ opacity: 0, translateX: 75 }}>
-        <p className="text-white text-5xl font-bold text-right">SCRIPT LIBRARY</p>
+        <p className="text-white text-3xl md:text-5xl font-bold text-right">SCRIPT LIBRARY</p>
       </AnimateDiv>
 
       <AnimateDiv initialProps={{ opacity: 0, translateX: -75 }}>
-        <div className="text-gray-600 text-2xl font-bold">SHARE OUR TOOLKIT</div>
-        <div>
-          <p className="text-m">We’ve posted some SQL Server scripts that we’ve written, found particularly handy, and have used for our clients with great success. Every DBA should stop in and check them out.</p>
-          <p className="text-m font-bold">Coming Soon: Sharepoint Office 365!</p>
+        <div className="flex flex-col gap-8">
+          <div className="text-gray-600 text-xl md:text-2xl font-bold">SHARE OUR TOOLKIT</div>
+          <div>
+            <p className="text-m">We’ve posted some SQL Server scripts that we’ve written, found particularly handy, and have used for our clients with great success. Every DBA should stop in and check them out.</p>
+            <p className="text-m font-bold">Coming Soon: Sharepoint Office 365!</p>
+          </div>
         </div>
       </AnimateDiv>
       <AnimateDiv initialProps={{ opacity: 0, translateX: 75 }}>
