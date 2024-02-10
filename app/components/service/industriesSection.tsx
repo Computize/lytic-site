911e4c775248746d646ee8a7d336e724a1a72510
Sam Bencivengo <sambencivengo@gmail.com>
2024-02-10 15:33:48 -0700

    industries section mobile compliant

--- app/components/service/industriesSection.tsx
+++ app/components/service/industriesSection.tsx
@@ -26,10 +26,13 @@ export const IndustriesSection = () => {
   return (
     <div className="text-white flex flex-col w-full h-auto py-20 bg-primary-green justify-center gap-8 items-center">
       <p className="text-5xl font-bold">INDUSTRIES</p>
-      <div className="grid grid-cols-5 w-full h-auto justify-center items-center">
+      <div className="flex flex-wrap gap-8 w-full h-auto justify-center items-center">
         {industriesImagesAndTitles.map(({ image, title }, idx) => {
           return (
-            <div key={idx} className="flex gap-8 flex-col justify-center items-center">
+            <div
+              key={idx}
+              className="flex w-60 my-4 gap-8 flex-col justify-center items-center"
+            >
               <img
                 src={`${subFolder}${image}`}
                 style={{
