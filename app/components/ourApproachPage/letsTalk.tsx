fb6943d43dd77104a3f646777a6d2b31a5504592
Sam Bencivengo <sambencivengo@gmail.com>
2024-02-10 13:17:06 -0700

    get our newsletter, need to dry this up

--- app/components/ourApproachPage/letsTalk.tsx
+++ app/components/ourApproachPage/letsTalk.tsx
@@ -1,10 +1,12 @@
-export const LetsTalk = ({ heading, bgColor }: { heading?: string; bgColor?: string }) => {
+import { cn } from '~/lib/utils';
+
+export const LetsTalk = ({ heading, bgColor, className }: { heading?: string; bgColor?: string; className?: string }) => {
   return (
-    <div className={`flex flex-col items-center justify-center gap-5 w-full ${bgColor ?? 'bg-primary-green'} h-52 mb-10`}>
-      <p className="text-5xl font-bold text-white">{heading ?? 'GET OUR NEWSLETTER'}</p>
-      <div className="flex flex-row gap-4">
+    <div className={cn(`flex flex-col items-center justify-center gap-5 w-full ${bgColor ?? 'bg-primary-green'} h-52 mb-10`, className)}>
+      <p className="text-center text-5xl font-bold text-white">{heading ?? 'GET OUR NEWSLETTER'}</p>
+      <div className="flex flex-col md:flex-row gap-4">
         <input
-          className="w-[800px] h-11 placeholder:italic pl-3"
+          className="w-[300px] md:w-[500px] h-11 placeholder:italic pl-3"
           placeholder="Enter your email address"
         />
         <button className={`bg-secondary-green px-9 text-white font-bold`}>CONTACT ME</button>
