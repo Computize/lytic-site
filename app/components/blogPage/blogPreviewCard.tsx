dfd89d0d88ecb78e2991f456e638e34d129c7049
Sam Bencivengo <sambencivengo@gmail.com>
2024-02-12 09:12:35 -0700

    blog previews

--- app/components/blogPage/blogPreviewCard.tsx
+++ app/components/blogPage/blogPreviewCard.tsx
@@ -1,20 +1,21 @@
 import { BlogItem } from '~/app/blog/page';
+import { Button } from '~/components/ui/button';
 
 interface BlogPreviewCardProps {
   blogItem: BlogItem;
 }
 
 export const BlogPreviewCard = ({ blogItem }: BlogPreviewCardProps) => {
-  const { content, date, previewText, title, author } = blogItem;
+  const { date, previewText, title, author } = blogItem;
   return (
-    <div className="min-w-[20px] max-w-[400px] max-h-[532px] border-[1px] flex flex-col gap-4 p-4">
+    <div className="min-w-[20px] max-w-[400px] border-[1px] flex flex-col items-start gap-4 p-4">
       <p className="text-3xl text-primary-green">{title}</p>
       <div>
         <p className="text-sm font-[500] text-gray-600">{author ?? 'Edward Heraux'}</p>
         <p className="text-sm text-gray-400">{date}</p>
       </div>
       <p className="text-xl line-clamp-6">{previewText}</p>
-      <button className="bg-primary-green">Read More</button>
+      <Button className="rounded-3xl bg-primary-green text-black">Read More</Button>
     </div>
   );
 };
