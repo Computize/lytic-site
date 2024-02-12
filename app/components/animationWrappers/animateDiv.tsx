99bb843968166aab1b07862f8809dd03fda1cb18
Sam Bencivengo <sambencivengo@gmail.com>
2024-02-12 15:48:01 -0700

    images and animation for what we do section

--- app/components/animationWrappers/animateDiv.tsx
+++ app/components/animationWrappers/animateDiv.tsx
@@ -1,16 +1,19 @@
 'use client';
 import { AnimationProps, motion } from 'framer-motion';
 import React from 'react';
+import { cn } from '~/lib/utils';
 
 interface AnimateDivProps {
   children: React.ReactNode;
   initialProps: AnimationProps['initial'];
   duration?: number;
   delay?: number;
+  className?: string;
 }
-export const AnimateDiv = ({ children, initialProps, duration, delay }: AnimateDivProps) => {
+export const AnimateDiv = ({ children, initialProps, duration, delay, className }: AnimateDivProps) => {
   return (
     <motion.div
+      className={cn(className)}
       initial={initialProps}
       viewport={{ once: true }}
       whileInView={{ opacity: 1, translateX: 0, translateY: 0, transition: { duration: duration ?? 1, delay: delay ?? 0 } }}
--- app/components/homePage/whatWeDo.tsx
+++ app/components/homePage/whatWeDo.tsx
@@ -1,16 +1,41 @@
+'use client';
+
+import { useScroll } from 'framer-motion';
+import { AnimateDiv } from '~/app/components/animationWrappers/animateDiv';
+
 export const WhatWeDo = () => {
+  const { scrollYProgress } = useScroll();
+  console.log(scrollYProgress);
+
   return (
     <div className="flex flex-col md:flex-row items-center justify-between h-auto gap-7 py-20">
       <div className="flex flex-col gap-3 md:gap-10 px-6 md:ml-40">
         <p className="text-primary-green text-4xl md:text-6xl font-bold">WHAT WE DO</p>
         <p className="text-l md:text-xl w-10/12">Specializing in Microsoft&apos;s analytics and data storage platform, high performance data warehouses and data lakes, and Microsoft&apos;s user-friendly ecosystem to surface it. We deliver on customized architecture that puts actionable data in your hands.</p>
       </div>
-      <div className="w-full flex justify-end">
+      <div className="w-full z-2 flex relative justify-end">
         <img
-          src="/what-we-do-main-img.png"
-          alt="Lytic Group Logo"
-          className="w-[300px] md:w-[2000px]"
+          src="/what-we-do-img.png"
+          alt="Stock Image Data"
+          className=" w-[300px] md:w-[2000px]"
         />
+        <AnimateDiv
+          className="absolute"
+          initialProps={{ translateX: 500 }}
+        >
+          <div>
+            <img
+              src="/what-we-do-lineart-img.png"
+              alt="Stock Image Data"
+              className="absolute z-10 w-[300px] md:w-[2000px]"
+            />
+            <img
+              src="/what-we-do-main-img.png"
+              alt="Stock Image Data"
+              className="z-0 w-[300px] md:w-[2000px]"
+            />
+          </div>
+        </AnimateDiv>
       </div>
     </div>
   );
