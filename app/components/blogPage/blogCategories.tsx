f8a6590dfcc1fff03015d4688c277d412c854812
Sam Bencivengo <sambencivengo@gmail.com>
2024-02-12 09:06:29 -0700

    blog even cleaner

--- app/components/blogPage/blogCategories.tsx
+++ app/components/blogPage/blogCategories.tsx
@@ -1,12 +1,28 @@
+import Link from 'next/link';
+
+export enum BlogCategory {
+  SHAREPOINT_OFFICE_365 = 'Sharepoint/Office 365',
+  CONSULTING = 'Consulting',
+  SQL_SERVER_DATABASE = 'SQL Server Database',
+  ANALYTICS_B_I = 'Analytics, B.I.',
+  ETL = 'ETL',
+}
+
 export function BlogCategories() {
   return (
-    <div className="flex flex-col w-auto gap-2">
-      <p className="text-xl">CATEGORIES:</p>
-      <p className="text-xl">dummy category</p>
-      <p className="text-xl">dummy category</p>
-      <p className="text-xl">dummy category</p>
-      <p className="text-xl">dummy category</p>
-      <p className="text-xl">dummy category</p>
+    <div className="flex flex-col w-3/12 gap-2">
+      <p className="text-xl font-bold">CATEGORIES:</p>
+      {Object.values(BlogCategory).map((category, idx) => {
+        return (
+          <Link
+            href={''}
+            key={idx}
+            className="text-xl hover:underline hover:font-bold"
+          >
+            {category}
+          </Link>
+        );
+      })}
     </div>
   );
 }
--- app/components/blogPage/blogPreviewCard.tsx
+++ app/components/blogPage/blogPreviewCard.tsx
@@ -8,7 +8,7 @@ export const BlogPreviewCard = ({ blogItem }: BlogPreviewCardProps) => {
   const { content, date, previewText, title, author } = blogItem;
   return (
     <div className="min-w-[20px] max-w-[400px] max-h-[532px] border-[1px] flex flex-col gap-4 p-4">
-      <p className="text-3xl text-primary-green">title</p>
+      <p className="text-3xl text-primary-green">{title}</p>
       <div>
         <p className="text-sm font-[500] text-gray-600">{author ?? 'Edward Heraux'}</p>
         <p className="text-sm text-gray-400">{date}</p>
