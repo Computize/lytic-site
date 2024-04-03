bf6fb6a3e95fa6f617ba074a8cbb63658da862eb
Sam Bencivengo <sambencivengo@gmail.com>
2024-04-03 13:01:53 -0600

    map updated

--- app/components/homePage/map.tsx
+++ app/components/homePage/map.tsx
@@ -1,3 +1,14 @@
 export const Map = () => {
-  return <div className="aspect-square w-[350px] md:w-[400px] border-2 border-red-500">MAP PLACEHOLDER</div>;
+  return (
+    <div className="aspect-square w-[350px] md:w-[400px]">
+      <img
+        className="h-auto"
+        style={{
+          objectFit: 'contain',
+        }}
+        src={'/nyc.png'}
+        alt={'map of nyc'}
+      />
+    </div>
+  );
 };
