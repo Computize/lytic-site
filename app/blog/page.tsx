353f9086b52e25e9fd30f47a7f851eeb4bda5020
Sam Bencivengo <sambencivengo@gmail.com>
2024-04-18 12:55:52 -0600

    all blogs fetched

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,17 +1,25 @@
+import React, { useState } from 'react';
 import { BlogPostCategory, Post, getPosts } from '~/lib/getPosts';
 import { BlogPreviewCardContainer } from '~/app/components/blogPage/blogPreviewCardContainer';
 import { PageTitle } from '~/app/components/pageTitle';
 import { PageUpperImageContainer } from '~/app/components/pageUpperImageContainer';
 import { generateMetadata } from '~/app/constants/pageMetadata';
-import React from 'react';
+import { BlogCategories } from '~/app/components/blogPage/blogCategories';
 
 export const metadata = generateMetadata('Blog');
 
+interface Params {
+  slug: string;
+}
+
+interface SearchParams {
+  [key: string]: string | string[] | undefined;
+}
+
 export type BlogPreview = Post;
 
-export default async function Page() {
-  const posts = await getPosts(BlogPostCategory.SHAREPOINT);
-  console.log(posts.length);
+export default async function Page({ params }: { params: Params; searchParams: SearchParams }) {
+  const posts = await getPosts();
   return (
     <main className="flex flex-col justify-center items-center w-full">
       <PageUpperImageContainer imageSource="/blog-page-banner.png">
@@ -22,6 +30,7 @@ export default async function Page() {
       </PageUpperImageContainer>
       <div className="py-10 p-6 flex flex-col sm:flex-col lg:flex-row justify-center gap-10 w-full md:w-10/12">
         <BlogPreviewCardContainer blogItems={posts} />
+        <BlogCategories />
       </div>
     </main>
   );
--- app/components/blogPage/blogCategories.tsx
+++ app/components/blogPage/blogCategories.tsx
@@ -1,23 +1,39 @@
-'use-client';
+'use client';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import { Button } from '~/components/ui/button';
-import { BlogPostCategory } from '~/lib/getPosts';
 
+export enum BlogPostCategory {
+  ALL = 'All',
+  SHAREPOINT = 'Sharepoint/Office 365',
+  CONSULTING = 'Consulting',
+  SQL = 'SQL Server Database',
+  ANALYTICS = 'Analytics, B.I.',
+  ETL = 'ETL',
+}
 export function BlogCategories() {
+  const router = useRouter();
   return (
     <div className="flex flex-col w-3/12 gap-2">
       <p className="text-xl font-bold">CATEGORIES:</p>
       {Object.values(BlogPostCategory).map((category, idx) => {
         return (
-          <Button key={idx}>
-            <Link
-              href={''}
+          <Link
+            href={''}
+            key={idx}
+            className="text-xl hover:underline hover:font-bold"
+          >
+            <Button
+              variant={'link'}
               key={idx}
+              onClick={() => {
+                router.push(`?category=${category}`);
+              }}
               className="text-xl hover:underline hover:font-bold"
             >
               {category}
-            </Link>
-          </Button>
+            </Button>
+          </Link>
         );
       })}
     </div>
