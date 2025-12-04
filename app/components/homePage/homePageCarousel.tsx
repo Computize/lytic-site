62e2ec2eb133a93f9b775b59acf6fb3a0624d0f8
ed <eheraux@yahoo.com>
2025-12-04 11:31:17 -0500

    Previous delay change didn't work. Changed autoPlay settings in Carousel.tsx

--- app/components/homePage/homePageCarousel.tsx
+++ app/components/homePage/homePageCarousel.tsx
@@ -76,8 +76,7 @@ export const HomePageCarousel = () => {
                     <AnimateDiv
                       viewPortOnce={false}
                       initialProps={{ opacity: '0', translateY: -50 }}
-                      // delay={0.75}
-                      delay={2.5}
+                      delay={0.75}
                     >
                       <p className="text-4xl md:text-5xl font-bold">{title}</p>
                     </AnimateDiv>
@@ -85,8 +84,8 @@ export const HomePageCarousel = () => {
                     <AnimateDiv
                       viewPortOnce={false}
                       initialProps={{ opacity: '0', translateX: -10 }}
-                      // delay={0.75}
-                      delay={2.5}
+                       delay={0.75}
+                      
                     >
                       <p className="text-white text-2xl">{description}</p>
                     </AnimateDiv>
@@ -94,8 +93,7 @@ export const HomePageCarousel = () => {
                     <AnimateDiv
                       viewPortOnce={false}
                       initialProps={{ opacity: '0', translateY: 50 }}
-                      // delay={0.75}
-                      delay={2.5}
+                      delay={0.75}
                     >
                       <Button className="bg-primary-green hover:bg-secondary-green w-auto h-auto border-b-4 border-green-100 text-white py-3 px-8 rounded-full">
                         <Link
