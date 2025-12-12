84744a3e6d256f44bc1d8d86af40bbc656421324
ed <eheraux@yahoo.com>
2025-12-12 16:22:12 -0500

    The previous change wasn't enough. Replaced the code in AboutUs' page.tsx to duplicate everything in the About page.

--- app/aboutUs/page.tsx
+++ app/aboutUs/page.tsx
@@ -4,18 +4,32 @@
 // This is only necessary because the QR code on current Lytic Group business cards
 // points to /aboutUs instead of /about
 
-// app/our-people-presidents-blog/page.tsx
-import React from 'react';
-import { OurPeoplePresidentsBlog } from '~/app/components/homePage/ourPeoplePresidentsBlog';
+// app/aboutUs/page.tsx
+import type { Metadata } from 'next';
+import { OurMission } from '~/app/components/about/ourMission';
+import { OurStory } from '~/app/components/about/ourStory';
+import { SomeOfOurClients } from '~/app/components/about/someOfOurClients';
+import { PageBlurb } from '~/app/components/pageBlurb';
+import { PageUpperImageContainer } from '~/app/components/pageUpperImageContainer';
+import { generateMetadata } from '~/app/constants/pageMetadata';
 
-export const metadata = {
-  title: "Our People & President's Blog",
-};
+export const metadata = generateMetadata('About Us');
 
 export default function Page() {
+  const blurbString = 'The Lytic Group are architects of data...';
   return (
-    <main className="flex flex-col items-center justify-center py-12">
-      <OurPeoplePresidentsBlog />
+    <main className="mb-14">
+      <PageUpperImageContainer imageSource="/aboutus-page-banner.png" className="h-[262px]">
+        <PageBlurb className="..." blurb={blurbString} />
+      </PageUpperImageContainer>
+      <div className="flex justify-center">
+        <OurStory />
+      </div>
+      <div className="flex justify-center mb-20">
+        <p className="text-xl text-center font-bold">“We lead with transparency...”</p>
+      </div>
+      <OurMission />
+      <SomeOfOurClients />
     </main>
   );
 }
