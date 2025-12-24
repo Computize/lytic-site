068c00f4268cf3dd05e0027702c28086accfe7bb
ed <eheraux@yahoo.com>
2025-12-24 16:13:10 -0500

    Deleted app/aboutus folder to really be able to start over with the AI instructions

--- app/aboutus/page.tsx
+++
@@ -1,9 +0,0 @@
-// app/aboutus/page.tsx
-import AboutPage from '~/app/about/page';
-import { generateMetadata } from '~/app/constants/pageMetadata';
-
-export const metadata = generateMetadata('About Us');
-
-export default function Page() {
-  return <AboutPage />;
-}
